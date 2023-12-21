fef90b478686e0b7a5cec23f906ddff090cc457a
ovidiuusva <150781665+ovidiuusva@users.noreply.github.com>
2023-12-21 15:27:21 +0200

    streams testing

--- lib/streams/number_duplicator.dart
+++ lib/streams/number_duplicator.dart
@@ -1,10 +1,16 @@
 import 'dart:async';
 
+import 'package:aparkado_interview/streams/backend.dart';
+
 class NumberDuplicator {
-  final _input = StreamController<int>();
+  final _input = StreamController<int>()
+    ..stream.listen((number) async {
+      final doubledNumber = await Backend().getDouble(number);
+      _output.add(doubledNumber);
+    });
 
   Stream<int> get output => _output.stream;
-  final _output = StreamController<int>();
+  static final _output = StreamController<int>();
 
   void addNumber(int number) => _input.add(number);
 }
--- test/number_duplicator_test.dart
+++ test/number_duplicator_test.dart
@@ -1,7 +1,12 @@
+import 'package:aparkado_interview/streams/number_duplicator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('should duplicate number', () {
-    // TODO
+    final myUTS = NumberDuplicator();
+    myUTS.output.listen((number) {
+      expect(number, 10);
+    });
+    myUTS.addNumber(5);
   });
 }
